49d5b1a6b9bdc721fd50ffc9a9de6c7f6780a134
yoshioka11 <yoshioka.yuuki11@gmail.com>
2017-01-18 01:37:56 +0900

    blog作成のためのrouting

--- express/blog/app.js
+++ express/blog/app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     logger = require('morgan'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    post = require('./routes/post');
 
 app.set('views',__dirname + '/views');
 app.set('view engine','ejs');
@@ -9,13 +10,18 @@ app.set('view engine','ejs');
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(express.methodOverride());
 app.use(logger('dev'));
-app.use(express.static(__dirname + '/public'));
 
-app.get('/new',function(req,res){
-  res.render('new');
-});
+//routing
+
+app.get('/', post.index);
+app.get('/posts/new', post.new);
+app.post('/posts/create', post.create);
+app.get('/posts/:id', post.show);
+app.get('/posts/:id/edit', post.edit);
+app.put('/posts/:id/', post.update);
+app.delete('/posts/:id/', post.destroy);
 
 app.post('/create',function(req,res){
   res.send(req.body.name);
